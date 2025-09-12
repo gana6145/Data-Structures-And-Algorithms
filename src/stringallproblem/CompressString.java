302dde48570f46d18eb520fbac1ebe43bdd3bf7a
Ganesh Jadhav <jadhav.ganesh1700@gmail.com>
2025-09-12 18:34:18 +0530

    solvedthe problem of String compession

---
+++ src/stringallproblem/CompressString.java
@@ -0,0 +1,34 @@
+package stringallproblem;
+
+public class CompressString {
+	
+	
+	public static String compressString(String str){
+		String ans="";
+		int count=1;
+		
+		for(int i=0,j=i+1;i<str.length();) {
+			if((j<str.length())&&(str.charAt(i)==str.charAt(j))) {
+				count++;
+				j++;
+			}
+			else {
+				ans=ans+str.charAt(i)+count;
+				i=j;
+				count=1;
+				j=i+1;
+			}
+		}
+		
+	return ans;
+		
+	}
+
+	public static void main(String[] args) {
+		String str="GGGAANNEESShhh";
+		System.out.println(compressString(str));
+		
+
+	}
+
+}
